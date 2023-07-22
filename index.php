66390e0c43b1426b60717617e6a9c207ec0355fe
nadaEssamElDin <101478063+nadaEssamElDin@users.noreply.github.com>
2023-07-22 18:03:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,25 +9,25 @@
     
     <form action="index2.php" method="post">
         <label for="first_name">First Name:</label>
-        <input type="text" name="first_name" id="first_name" required placeholder="Enter your first name"><br>
+        <input type="text" name="first_name" id="first_name"  placeholder="Enter your first name"><br>
 
         <label for="last_name">Last Name:</label>
-        <input type="text" name="last_name" id="last_name" required placeholder="Enter your last name"><br>
+        <input type="text" name="last_name" id="last_name"  placeholder="Enter your last name"><br>
 
         <label for="address">Address:</label>
-        <input type="text" name="address" id="address" required placeholder="Enter your address"><br>
+        <input type="text" name="address" id="address"  placeholder="Enter your address"><br>
 
         <label for="country">Country:</label>
-        <select name="country" id="country" required>
+        <select name="country" id="country" >
             <option value="">Select a country</option>
-            <option value="country1">EGYPT</option>
-            <option value="country2">USA</option>
+            <option value="EGYPT">EGYPT</option>
+            <option value="USA">USA</option>
           
         </select><br>
 
         <label for="gender">Gender:</label>
-        <input type="radio" name="gender" value="male" required> Male
-        <input type="radio" name="gender" value="female" required> Female<br>
+        <input type="radio" name="gender" value="male" > Male
+        <input type="radio" name="gender" value="female" > Female<br>
 
         <label>Skills:</label>
         <input type="checkbox" name="skills[]" value="PHP"> PHP
@@ -38,17 +38,17 @@
 
         <br>
         <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required placeholder="Enter your username"><br>
+        <input type="text" name="username" id="username"  placeholder="Enter your username"><br>
 
         <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required placeholder="Enter your password"><br>
+        <input type="password" name="password" id="password"  placeholder="Enter your password"><br>
 
         <label for="department">Department:</label>
-        <input type="text" name="department" id="department" required placeholder="Computer Science"><br><br>
+        <input type="text" name="department" id="department"  placeholder="Computer Science"><br><br>
                  
                      sh688c  <br>
         <label for="code">Enter this code :</label>
-        <input type="text" name="code" id="code" required><br>
+        <input type="text" name="code" id="code" ><br>
 
         <input type="submit" value="Submit">
         <input type="reset" value="Reset">
